337809fe84f9e99da94f5f23e476626ca4d26032
Marcos Filipe Gomes Da Silva Lino <marcos.lino@b2wdigital.com>
2019-08-05 09:38:21 -0300

    pipenv use

--- install.sh
+++ install.sh
@@ -1,4 +1,2 @@
 wget https://github.com/gdrive-org/gdrive/releases/download/2.1.0/gdrive-linux-x64
-mv gdrive-linux-x64 gdrive
-
-./gdrive download -r 11UjjkpGt5reLBzFqGjoGAbN-JAqEOzJO
+mv gdriver-linux-x64 gdriver
