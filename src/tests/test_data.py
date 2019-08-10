8b760ea90712a7a9735b390020625263e8ad4086
Marcos Filipe Lino <mfilipelino@gmail.com>
2019-08-10 16:16:19 -0300

    pydata

--- src/tests/test_data.py
+++ src/tests/test_data.py
@@ -1,17 +1,12 @@
 from unittest import TestCase
 from data import load, image, audio
 
-from os import path
-
 
 class DataTest(TestCase):
 
     def test_load(self):
         verbs = load('verbs')
         self.assertTrue(len(verbs) > 0)
-
-    def test_x(self):
-        self.assertTrue(path.isfile('/home/marcosfilipe/dev/github/english-notebook/media/images/go.jpg'))
 
     def test_is_images_exist(self):
         self.assertEqual(image('go'), (True, 'go.jpg'))
